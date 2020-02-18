ce6b3fae3a7bd1c62729c2e3917ff13921f9302f
Sneha Ojha <42754345+osneha@users.noreply.github.com>
2020-02-18 00:19:03 -0600

    Add files via upload

---
+++ b.js
@@ -0,0 +1,13 @@
+function b()
+{
+     const Total=parseFloat(document.querySelector('#total').value)
+     const Rate=parseFloat(document.querySelector('#rate').value)
+     console.log(Rate);
+     const Tip=Total*(Rate/100)
+     const Tax=Total*(5.5/100)
+     const GrandTotal=Total+Tip+Tax;
+     document.querySelector('#tip').innerHTML=`$${Tip.toFixed(2)}`
+     document.querySelector('#tax').innerHTML=`$${Tax.toFixed(2)}`
+     document.querySelector('#grand-total').innerHTML=`$${GrandTotal.toFixed(2)}`
+}
+     document.querySelector('#btn').addEventListener('click',b)
